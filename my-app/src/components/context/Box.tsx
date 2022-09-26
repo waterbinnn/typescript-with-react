c2bfd35d5eb09f26a33aaea600ca2c72e45ed629
waterbinnn <ewaterbin@naver.com>
2022-09-26 17:27:29 +0900

    💄 컨텍스트 이용하여 스타일 작성

--- my-app/src/components/context/Box.tsx
+++ my-app/src/components/context/Box.tsx
@@ -1,3 +1,17 @@
+import { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
+
 export const Box = () => {
-    return <div>Theme context</div>;
+    const theme = useContext(ThemeContext);
+
+    return (
+        <div
+            style={{
+                backgroundColor: theme.primary.main,
+                color: theme.primary.text,
+            }}
+        >
+            Theme context
+        </div>
+    );
 };
